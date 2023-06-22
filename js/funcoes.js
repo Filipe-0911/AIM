d6cfbd77338988ad22d0fc94cc785aa69c82e941
Filipe <fbianchi.andrade@gmail.com>
2023-06-22 14:31:28 -0300

    testando reconhecimento do checkbox

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,34 +1,10 @@
-function alterarQtd(produto, acao){
-    const qtd = document.getElementById('qtd_' + produto)
-    const valor = document.getElementById('valor_' + produto)
-    const total = document.getElementById('total_' + produto)
+var checkbox = document.getElementById('solicitouEsclarecimento');
+ 
+checkbox.addEventListener('click', function () {
 
-    if(acao == '-' && qtd.innerHTML == 0){
-
-        alert('Atenção! A quantidade não pode ser menor que 0.')
+    if(checkbox.checked) {
+        console.log(checkbox.checked);
     } else {
-    
-    acao == '+' ?  qtd.innerHTML++ : qtd.innerHTML--
-    const valorTotal = qtd.innerHTML * somenteNumeros(valor.innerHTML)
-    total.innerHTML = formatarValor(valorTotal)
-    soma()
+        console.log(checkbox.checked);
     }
-}
-
-function soma(){
-    let soma = 0
-    
-    for(let i = 1; i<4; i++){
-        let numero = somenteNumeros(document.getElementById('total_' + i).innerHTML)
-        soma +=  Number(numero)
-    }
-
-    document.getElementById('subtotal').innerHTML = formatarValor (soma)
-}
-
-function somenteNumeros(n){
-    return n.replace(/\D/g, '')
-}
-function formatarValor(n){
-    return 'R$' + n.toLocaleString('pt-BR')
-}
+})
